11aaf1a796d7c4c5161dc8999d39c27742d36a55
Tim Dorr <timdorr@timdorr.com>
2016-08-05 13:04:32 -0400

    Add logging to stdout with common log format.

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const morgan = require('morgan')
 const isDomain = require('is-fqdn')
 const parseURL = require('./parseURL')
 
@@ -8,7 +9,11 @@ const web = require('./sources/web')
 const sendImage = require('./sendImage')
 const { send404, sendError } = require('./errors')
 
+const logger = morgan('combined')
+
 module.exports = http.createServer((req, res) => {
+  logger(req, res, () => {})
+
   const url = parseURL(req.url)
 
   if (!url) return send404(res)
