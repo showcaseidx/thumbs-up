916b0f3d854cd529e370aa6117e9c60c1561270b
Scott Davis <me@sdavis.info>
2019-07-06 03:15:52 -0400

    add conditional logic for trestle urls

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,11 @@
 const got = require('got')
 
-module.exports = (res, server, path, query) =>
-  got(`http://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null })
-    .then(response => response.body)
+module.exports = (res, server, path, query) => {
+  if (server == 'api-prod.corelogic.com') {
+    return got(`https://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null })
+      .then(response => response.body)
+  } else {
+    return got(`http://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null })
+      .then(response => response.body)
+  }
+}
