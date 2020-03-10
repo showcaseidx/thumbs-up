ae57b4c8befedf4a81279f9f3bf84e6fd467363c
Scott Davis <me@sdavis.info>
2020-03-10 10:51:05 -0400

    dont fail if the ssl cert chain is bad

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@ const got = require('got')
 
 module.exports = (res, server, path, query) => {
   if (server == 'api-prod.corelogic.com') {
-    return got(`https://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null })
+    return got(`https://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
       .then(response => response.body)
   } else {
     return got(`http://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null })
