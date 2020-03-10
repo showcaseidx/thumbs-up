4cd1cb0370a204e44a3d9321cf270b544eea6ea4
Scott Davis <me@sdavis.info>
2020-03-10 11:05:04 -0400

    dont reject

--- src/fetch.js
+++ src/fetch.js
@@ -5,7 +5,7 @@ module.exports = (res, server, path, query) => {
     return got(`https://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
       .then(response => response.body)
   } else {
-    return got(`http://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null })
+    return got(`http://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
       .then(response => response.body)
   }
 }
