c9b56fcbaac39875046a775bd7fadb8c3dd61cb8
Scott Davis <me@sdavis.info>
2020-03-24 14:54:38 -0400

    add sabor to ssl fetch

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,7 @@
 const got = require('got')
 
 module.exports = (res, server, path, query) => {
-  if (server == 'api-prod.corelogic.com' || server == 'matrixrets.ntreis.net') {
+  if (server == 'api-prod.corelogic.com' || server == 'matrixrets.ntreis.net' || server == 'sabor-media.cdn-connectmls.com') { 
     return got(`https://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
       .then(response => response.body)
   } else {
