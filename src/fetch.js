a9e550eb4a7350f8f13892aa5c480c5be73558cd
Scott Davis <me@sdavis.info>
2020-03-26 13:22:40 -0400

    debug

--- src/fetch.js
+++ src/fetch.js
@@ -1,10 +1,11 @@
 const got = require('got')
 
 module.exports = (res, server, path, query) => {
+  let remote_server = server
   if (server == 'http://api-prod.corelogic.com' || server == 'http://matrixrets.ntreis.net' || server == 'http://sabor-media.cdn-connectmls.com') {
-    server = server.replace('http://', 'https://');
+    remote_server = server.replace('http://', 'https://');
   }
- console.log(`trying to fetch: ${server}/${path}${query ? `?${query}` : ''}`)
-  return got(`${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
+ console.log(`trying to fetch: ${remote_server}/${path}${query ? `?${query}` : ''}`)
+  return got(`${remote_server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
     .then(response => response.body)
 }
