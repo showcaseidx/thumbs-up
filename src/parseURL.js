055bb83feb0fa90e8a81355baf59ba4781784a14
Scott Davis <me@sdavis.info>
2020-03-26 12:40:18 -0400

    array is totaly shifted by one index because of added /s/

--- src/parseURL.js
+++ src/parseURL.js
@@ -6,16 +6,21 @@ module.exports = str => {
   const [ url, query ] = str.split('?')
   const parts = url.match(splitter)
 
+  // www.resizer-domain.com/s/10x10/imagesourcefrommls.com
   if (parts)
-    if (parts[0] && parts[0] == 's') {
-      parts.shift()
-      parts[2] = `https://${parts[2]}`
-    }
-
-    return {
-      dimensions: parseDimensions(parts[1]),
-      server: `http://${parts[2]}`,
-      path: parts[3],
-      query,
+    if (parts[1] && parts[1] == 's') {
+      return {
+        dimensions: parseDimensions(parts[2]),
+        server: `https://${parts[3]}`,
+        path: parts[4],
+        query,
+      }
+    } else {
+      return {
+        dimensions: parseDimensions(parts[1]),
+        server: `http://${parts[2]}`,
+        path: parts[3],
+        query,
+      }
     }
 }
