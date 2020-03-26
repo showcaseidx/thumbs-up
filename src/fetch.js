9af764d61a597489ffdcd290ef3fad290fb9acbb
Scott Davis <me@sdavis.info>
2020-03-26 12:27:04 -0400

    detect prepended /s/ for https urls

--- src/fetch.js
+++ src/fetch.js
@@ -1,11 +1,10 @@
 const got = require('got')
 
 module.exports = (res, server, path, query) => {
-  if (server == 'api-prod.corelogic.com' || server == 'matrixrets.ntreis.net' || server == 'sabor-media.cdn-connectmls.com') { 
-    return got(`https://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
-      .then(response => response.body)
-  } else {
-    return got(`http://${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
-      .then(response => response.body)
+  if (server == 'http://api-prod.corelogic.com' || server == 'http://matrixrets.ntreis.net' || server == 'http://sabor-media.cdn-connectmls.com') {
+    server = server.replace('http://', 'https://');
   }
+
+  return got(`${server}/${path}${query ? `?${query}` : ''}`, { encoding: null, rejectUnauthorized: false })
+    .then(response => response.body)
 }
--- src/parseURL.js
+++ src/parseURL.js
@@ -7,9 +7,14 @@ module.exports = str => {
   const parts = url.match(splitter)
 
   if (parts)
+    if (parts[0] && parts[0] == 's') {
+      parts.shift()
+      parts[2] = `https://${parts[2]}`
+    }
+
     return {
       dimensions: parseDimensions(parts[1]),
-      server: parts[2],
+      server: `http://${parts[2]}`,
       path: parts[3],
       query,
     }
