a46523f5eed2dccd6b62eabb4e5652d5ae7ff827
Scott Davis <me@sdavis.info>
2020-03-26 13:13:57 -0400

    redo url parsing

--- src/parseURL.js
+++ src/parseURL.js
@@ -1,29 +1,23 @@
 const parseDimensions = dimensions => dimensions.split('x', 2).map(d => parseInt(d, 10) || null)
 
-const splitter = new RegExp('^/([^/]+)/([^/]+)/(.+)$')
+const unsecure = new RegExp('^/([^/]+)/([^/]+)/(.+)$')
+const secure = new RegExp('^/s/([^/]+)/([^/]+)/(.+)$')
+const secure_url = new RegExp('^/s/')
 
 module.exports = str => {
   const [ url, query ] = str.split('?')
-  const parts = url.match(splitter)
+  let regex = unsecure
 
   // www.resizer-domain.com/s/10x10/imagesourcefrommls.com
-  if (parts)
-    console.log(parts)
-    if (parts[1] && parts[1] == 's') {
-      console.log('https')
-      return {
-        dimensions: parseDimensions(parts[2]),
-        server: `https://${parts[3]}`,
-        path: parts[4],
-        query,
-      }
-    } else {
-      console.log('http')
-      return {
-        dimensions: parseDimensions(parts[1]),
-        server: `http://${parts[2]}`,
-        path: parts[3],
-        query,
-      }
-    }
+  if (url.match(secure_url)) {
+    regex = secure
+  }
+  const parts = url.match(regex)
+
+  return {
+    dimensions: parseDimensions(parts[1]),
+    server: `http://${parts[2]}`,
+    path: parts[3],
+    query,
+  }
 }
