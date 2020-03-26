2950d131834902291511748573806bb8122c1b1f
Scott Davis <me@sdavis.info>
2020-03-26 13:18:59 -0400

    redo url parsing

--- src/parseURL.js
+++ src/parseURL.js
@@ -7,16 +7,18 @@ const secure_url = new RegExp('^/s/')
 module.exports = str => {
   const [ url, query ] = str.split('?')
   let regex = unsecure
+  let proto = 'http://'
 
   // www.resizer-domain.com/s/10x10/imagesourcefrommls.com
   if (url.match(secure_url)) {
     regex = secure
+    proto = 'https://'
   }
   const parts = url.match(regex)
 
   return {
     dimensions: parseDimensions(parts[1]),
-    server: `http://${parts[2]}`,
+    server: `${proto}${parts[2]}`,
     path: parts[3],
     query,
   }
